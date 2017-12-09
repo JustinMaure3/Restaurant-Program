2341f71fff2199f2938d1cec68f958acf069e22d
Stefano Iaconetti <stefano.iaconetti01@stclairconnect.ca>
2017-12-09 10:00:29 -0500

    #4 Started working on the database but we have yet to learn how to complete it so i will finish this after lecture

--- src/Database/Database.java
+++ src/Database/Database.java
@@ -1,7 +1,51 @@
 package Database;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import com.mysql.jdbc.Connection;
+
 public class Database {
 
+	//Create an instance variable
+		private static Database instance = null;
+		private Connection connection = null;
+		
+	//Private constructor
+	private Database() {
+		//If the connection exists
+		if(connection == null) {
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+				//connection = DriverManager.getConnection("jdbc:mysql://php.scweb.ca/" + Const.DB_NAME +"?useSSL=false", Const.DB_USER, Const.DB_PASS);
+				System.out.println("Connection Sucessfully Created");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
 	
+	//getInstance method
+		public static Database getInstance() {
+			if(instance == null) {
+				instance = new Database();
+			}
+			return instance;
+		}
+	
+	public Connection getConnection() {
+		return connection;
+	}
+	
+	//Close connection method
+	public void close() {
+		System.out.println("Closing Connection");
+		try {
+			connection.close();
+		}catch (SQLException e) {
+			connection = null;
+			e.printStackTrace();
+		}
+	}
 	
 }
