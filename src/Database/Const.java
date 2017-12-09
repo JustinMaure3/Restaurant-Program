80a6b75de47aefff5947a7a791f4fdd7d63b562d
Stefano Iaconetti <stefano.iaconetti01@stclairconnect.ca>
2017-12-09 09:37:06 -0500

    #4 Made a mistake and added my name,user,pass to database instead of const so I fixed this issue

--- src/Database/Const.java
+++ src/Database/Const.java
@@ -1,5 +1,8 @@
 package Database;
 
 public class Const {
-
+	//My name user and password for the server
+	public static final String DB_NAME = "siaconetti968db";
+	public static final String DB_USER = "siaconetti968";
+	public static final String DB_PASS = "q8hccq8hcc6pv7d6pv7d";
 }
