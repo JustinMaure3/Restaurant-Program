7fcea6a8fe5916b44c003c0878249a43211cb89b
Justin Maure <justin.maure01@stclairconnect.ca>
2017-12-09 11:07:53 -0500

    #22 Working with tabs having issues

---
+++ src/tabs/HomeTab.java
@@ -0,0 +1,18 @@
+package tabs;
+
+import javafx.scene.control.Tab;
+import javafx.stage.Stage;
+
+public class HomeTab extends Tab {
+
+	public HomeTab() {
+		this.setText("Home");
+	}
+	
+//	Main.mainStage.setScene(new HomeScene());
+//	
+//	public void start(Stage homeStage) throws Exception {
+//		homeStage.setScene(new HomeScene());
+//	}
+	
+}
