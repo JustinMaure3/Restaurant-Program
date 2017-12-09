046652e0a0c26a0c2f06b2b1deabdfc2df754b03
Stefano Iaconetti <stefano.iaconetti01@stclairconnect.ca>
2017-12-09 09:35:54 -0500

    #4 I created the classes Const and Database in a Databse package and added to Database my dbname,user,pass

---
+++ src/Database/Database.java
@@ -0,0 +1,9 @@
+package Database;
+
+public class Database {
+
+	public static final String DB_NAME = "siaconetti968db";
+	public static final String DB_USER = "siaconetti968";
+	public static final String DB_PASS = "q8hccq8hcc6pv7d6pv7d";
+	
+}
