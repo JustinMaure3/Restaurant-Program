8954f2812588298aa058ff5edd068d2536374003
Max Fontes <max.fontes01@stclairconnect.ca>
2017-12-11 12:00:16 -0500

    Fixed the constructor methods for the food and drinks.

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,26 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	
+	
 
 	public static Stage mainStage;
 	
 	public static void main(String[] args) {
 		Application.launch(args);
+		
+//		private String name;
+//		private String description;
+//		private String picture;
+//		private int rating;
+		
+		
 
 	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		//Mainstage is set up
+		//Main stage is set up
 		mainStage = primaryStage;
 		mainStage.setResizable(false);
 		mainStage.setScene(new HomeScene());
