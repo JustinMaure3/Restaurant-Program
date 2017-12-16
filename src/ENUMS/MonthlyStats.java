12813dcbb53eeaed9e68d2dfe9b756c93b97e2b6
Tomas Markos <tomas.markos01@stclairconnect.ca>
2017-12-16 13:42:21 -0500

    #65 Added the text for the ComboBox and made it show the text Monthly Stats

--- src/ENUMS/MonthlyStats.java
+++ src/ENUMS/MonthlyStats.java
@@ -11,6 +11,7 @@ public enum MonthlyStats {
 	JULY,
 	AUGUST,
 	SEPTEMBER,
+	OCTOBER,
 	NOVEMBER,
 	DECEMBER
 	
--- src/tabs/StatisticsTab.java
+++ src/tabs/StatisticsTab.java
@@ -1,10 +1,27 @@
 package tabs;
 
+import javafx.geometry.Insets;
 import javafx.scene.control.Tab;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
 
 public class StatisticsTab extends Tab{
 
 	public StatisticsTab() {
 		this.setText("Statistics");
+		
+		//This GridPane will contain the content
+		GridPane pane = new GridPane();
+		
+		//Text for the ComboBox named monthBox "Monthly Stats"
+		Text monthBox = new Text("Monthly Stats");
+		pane.add(monthBox, 0, 0);
+		
+		
+		
+		pane.setPadding(new Insets(10,10,10,10));
+		pane.setVgap(10);
+		pane.setHgap(10);
+		this.setContent(pane);
 	}
 }
