8e175e8016470306e42140876518403cfd81f534
Stefano Iaconetti <stefano.iaconetti01@stclairconnect.ca>
2017-12-16 11:27:58 -0500

    #52 Created a basic title with the name of the restaurant

--- src/tabs/HomeTab.java
+++ src/tabs/HomeTab.java
@@ -1,11 +1,49 @@
 package tabs;
 
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
 
 public class HomeTab extends Tab {
 
 	public HomeTab() {
 		this.setText("Home");
+		
+		Label restName = new Label("Dinner Diner");
+		Label popName = new Label();
+		
+		Button next = new Button("Next");
+		Button prev = new Button("Prev");
+		
+		Text desc = new Text();
+		
+		HBox title = new HBox();
+		
+		
+		title.getChildren().add(restName);
+		this.setContent(title);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 	
 }
