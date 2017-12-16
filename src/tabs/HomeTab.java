ae3f8aa63bc6cba16c8a1caf883d1587db18c33e
Stefano Iaconetti <stefano.iaconetti01@stclairconnect.ca>
2017-12-16 11:44:08 -0500

    #52 added some padding, set the title to the top and enlarged the font

--- src/tabs/HomeTab.java
+++ src/tabs/HomeTab.java
@@ -1,10 +1,14 @@
 package tabs;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
 import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
 import javafx.scene.text.Text;
 
 public class HomeTab extends Tab {
@@ -12,18 +16,30 @@ public class HomeTab extends Tab {
 	public HomeTab() {
 		this.setText("Home");
 		
-		Label restName = new Label("Dinner Diner");
-		Label popName = new Label();
+		//Restaurant name and the name of the popular food
+		Text restName = new Text("Dinner Diner");
+		Text popName = new Text();
 		
+		//Buttons that will let you go through the foods
 		Button next = new Button("Next");
 		Button prev = new Button("Prev");
 		
+		//description of the Restaurant
 		Text desc = new Text();
 		
+		//HBox containing the title of the restaurant
 		HBox title = new HBox();
 		
-		
+		//Font specifically for the title
+		Font titleFont = Font.font("Times New Roman", FontPosture.REGULAR, 30);
+		restName.setFont(titleFont);
+		//Adding the title to the HBox then setting some padding and setting the title to the center top
 		title.getChildren().add(restName);
+		title.setAlignment(Pos.TOP_CENTER);
+		title.setPadding(new Insets(25, 0, 0, 0));
+		
+		
+		
 		this.setContent(title);
 		
 		
