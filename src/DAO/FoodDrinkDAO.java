b02a65f70152b89af78242772238b7c9d2bb355f
Stefano Iaconetti <stefano.iaconetti01@stclairconnect.ca>
2017-12-16 10:26:20 -0500

    #48 I created everything we wanted to do with the database which includes getall, get, update, delete and create

---
+++ src/DAO/FoodDrinkDAO.java
@@ -0,0 +1,20 @@
+package DAO;
+
+import java.util.ArrayList;
+
+import PlaceHolder.FoodDrink;
+
+public interface FoodDrinkDAO {
+	//Everything we want to do with the database
+	//Get all
+	public ArrayList<FoodDrink> getAllFoodDrink();
+	//Get 1
+	public FoodDrink getFoodDrink(int itemID);
+	//Update
+	public void updateFoodDrink(FoodDrink foodDrink);
+	//Delete
+	public void deleteFoodDrink(FoodDrink foodDrink);
+	//Create
+	public void createFoodDrink(FoodDrink foodDrink);
+	
+}
