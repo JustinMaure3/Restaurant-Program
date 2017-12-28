235758490c141dd5a24091cdc0a6bb9c0abf19d0
Stefano Iaconetti <stefano.iaconetti01@stclairconnect.ca>
2017-12-28 10:33:02 -0500

    #83 created the basic layout of the hometab with all V and H boxes in order so when i get the content it would take me just a brief moment to apply the changes without any apperance issues

--- src/tabs/HomeTab.java
+++ src/tabs/HomeTab.java
@@ -19,29 +19,42 @@ public class HomeTab extends Tab {
 		
 		//Restaurant name and the popular foodname and employee of the month 
 		Text restName = new Text("Dinner Diner");
-		Text popName = new Text();
-		Text empName = new Text();
+		Text popName = new Text("TOP FOOD TEST");//Here would grab the most popular food item name
+		Text empName = new Text("TOP EMPLOYEE TEST");
 		
 		//Tip for the restaurant
-		Text tipText = new Text();
+		Text tipText = new Text("RANDOM TIP I WILL BE GENERATING VERY SHORTLY");
 		
 		//HBox containing the title of the restaurant
 		VBox main = new VBox();
 		HBox title = new HBox();
 		HBox empFood = new HBox();
 		HBox tip = new HBox();
+		VBox Vemp = new VBox();
+		VBox Vfood = new VBox();
 		
 		//Font specifically for the title
 		Font titleFont = Font.font("Times New Roman", FontPosture.REGULAR, 30);
 		restName.setFont(titleFont);
+		
+		
 		//Adding the title to the HBox then setting some padding and setting the title to the center top
 		title.getChildren().add(restName);
 		title.setAlignment(Pos.TOP_CENTER);
 		title.setPadding(new Insets(25, 0, 0, 0));
 		
+		//This is where we will add the employee name and picture and food name and picture
+		Vemp.getChildren().add(empName);//THIS WILL ADD EMP PICTURE WHEN AVAILABLE
+		Vfood.getChildren().add(popName);//THIS WILL ADD POP FOOD AND PICTURE WHEN AVAILABLE
+		empFood.getChildren().addAll(Vemp, Vfood);
+		empFood.setAlignment(Pos.CENTER);
 		
 		
-		this.setContent(title);
+		//Everything will be added to the main and it will go from top to bottom
+		main.getChildren().addAll(title,empFood,tip);
+		
+		
+		this.setContent(main);
 		
 		
 		
