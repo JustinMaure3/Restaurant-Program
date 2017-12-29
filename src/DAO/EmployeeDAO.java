4dba82cb9e858322650b29b600d76a42163bf67e
Stefano Iaconetti <stefano.iaconetti01@stclairconnect.ca>
2017-12-29 12:04:26 -0500

    #85 Created the DAO time to work on the placeholder to erase these errors/issues

--- src/DAO/EmployeeDAO.java
+++ src/DAO/EmployeeDAO.java
@@ -1,10 +1,24 @@
 package DAO;
 
+import java.util.ArrayList;
+
+import PlaceHolder.FoodDrink;
+
 public class EmployeeDAO {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//Everything we want to do with the table
+		public ArrayList<Employees> getAllEmployees();
+		//Get 1
+		public FoodDrink getEmployee(int itemID);
+		//Update
+		public void updateEmployee(Employee employee);
+		//Delete
+		public void deleteEmployee(Employee employee);;
+		//Create
+		public void createEmployee(Employee employee);
+		//Gets the employee with the most stars
+		FoodDrink getEmployeeOfMonthk(int goodStars);
 	}
 
 }
