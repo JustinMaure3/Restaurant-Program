69f6dc4827c8a3120893f6659e9bc901448539bb
Justin Maure <justin.maure01@stclairconnect.ca>
2017-12-29 13:15:29 -0500

    #88 Created a listView to show all records in the database for the remove item tab

--- src/tabs/RemoveItemTab.java
+++ src/tabs/RemoveItemTab.java
@@ -1,6 +1,13 @@
 package tabs;
 
+import java.util.ArrayList;
+
+import PlaceHolder.FoodDrink;
+import Tables.FoodTable;
+import javafx.collections.FXCollections;
+import javafx.scene.control.ListView;
 import javafx.scene.control.Tab;
+import javafx.scene.layout.BorderPane;
 
 public class RemoveItemTab extends Tab {
 	
@@ -9,12 +16,18 @@ public class RemoveItemTab extends Tab {
 	private RemoveItemTab() {
 		this.setText("Remove Item");
 		
+		//Create the layout
+		BorderPane pane = new BorderPane();
+		//Create a listVuew thatll hold a bunch of instances of food and drink
+		ListView<FoodDrink> list = new ListView();
+		//Create a food table
+		FoodTable fTable = new FoodTable();
+		//Create the array that'll give us all foodDrink in our database
+		ArrayList<FoodDrink> foodDrinkItems = fTable.getAllFoodDrink();
 		
-		
-		
-		
-		
-		
+		//set the list with all the items in the arraylist
+		list.setItems(FXCollections.observableArrayList(foodDrinkItems));
+		pane.setCenter(list);
 		
 		
 		
