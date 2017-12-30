7201807ed2a35be5f5d17986fed8d7364fd25dda
Tomas Markos <tomas.markos01@stclairconnect.ca>
2017-12-30 12:14:06 -0500

    #42 Started creating the method for the PieChart

--- src/tabs/StatisticsTab.java
+++ src/tabs/StatisticsTab.java
@@ -5,7 +5,7 @@ package tabs;
 import javafx.collections.FXCollections;
 
 import javafx.geometry.Insets;
-
+import javafx.scene.chart.PieChart;
 import javafx.scene.control.Button;
 
 import javafx.scene.control.ComboBox;
@@ -25,49 +25,22 @@ public class StatisticsTab extends Tab{
 	private StatisticsTab() {
 
 		this.setText("Statistics");
-
-		
-
 		//This GridPane will contain the content
-
 		GridPane pane = new GridPane();
-
-		
-
 		//Text for the ComboBox named monthBox "Monthly Stats"
-
 		Text monthBoxText = new Text("Monthly Stats: ");
-
 		pane.add(monthBoxText, 0, 0);
-
-		
-
 		//ComboBox for the MonthlyStats
-
 		ComboBox<ENUMS.MonthlyStats> monthComboBox = new ComboBox<>();
-
-		
-
 		//Set the months to the monthComboBox
-
 		monthComboBox.setItems(FXCollections.observableArrayList(ENUMS.MonthlyStats.values()));
-
 		pane.add(monthComboBox, 1, 0);
-
 		
-
 		Button goBtn = new Button("GO");
-
 		pane.add(goBtn, 0, 1);
-
-		
-
 		pane.setPadding(new Insets(10,10,10,10));
-
 		pane.setVgap(10);
-
 		pane.setHgap(10);
-
 		this.setContent(pane);
 
 	}
@@ -77,5 +50,14 @@ public class StatisticsTab extends Tab{
 		}
 		return tab;
 	}
+	
+	
+	public static PieChart generateChart() {
+		
+		
+		return null;
+	}
+	
+	
 
 }
