b15704b12f0bbceb0a18f3bae719fe0ae9957069
Stefano Iaconetti <stefano.iaconetti01@stclairconnect.ca>
2017-12-30 18:04:03 -0500

    #101 completed the Store locataions DAO

--- src/DAO/StoreLocationDAO.java
+++ src/DAO/StoreLocationDAO.java
@@ -1,16 +1,17 @@
 package DAO;
 
 import java.util.ArrayList;
+import PlaceHolder.StoreLocations;
 
 public interface StoreLocationDAO {
-//	//Everything we want to do with the table
-//	public ArrayList<Manager> getAllManagers();
-//	//Get 1
-//	public Manager getManager(int itemID);
-//	//Update
-//	public void updateManager(Manager manager);
-//	//Delete
-//	public void deleteManager(Manager manager);;
-//	//Create
-//	public void createManager(Manager manager);
+	//Everything we want to do with the table
+	public ArrayList<StoreLocations> getAllStoreLocations();
+	//Get 1
+	public StoreLocations getStoreLocation(int itemID);
+	//Update
+	public void updateStoreLocation(StoreLocations storeLocations);
+	//Delete
+	public void deleteStoreLocation(StoreLocations storeLocations);;
+	//Create
+	public void createStoreLocation(StoreLocations storeLocations);
 }
