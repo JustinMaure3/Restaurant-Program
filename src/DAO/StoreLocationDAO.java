c7cc72e58c4eeff51f71afac55b1935d6af0c45d
Stefano Iaconetti <stefano.iaconetti01@stclairconnect.ca>
2017-12-30 17:05:20 -0500

    #101 layout for the StoreLocations DAO is made will come back after placeholder and table is made

--- src/DAO/StoreLocationDAO.java
+++ src/DAO/StoreLocationDAO.java
@@ -1,5 +1,16 @@
 package DAO;
 
-public class StoreLocationDAO {
+import java.util.ArrayList;
 
+public interface StoreLocationDAO {
+//	//Everything we want to do with the table
+//	public ArrayList<Manager> getAllManagers();
+//	//Get 1
+//	public Manager getManager(int itemID);
+//	//Update
+//	public void updateManager(Manager manager);
+//	//Delete
+//	public void deleteManager(Manager manager);;
+//	//Create
+//	public void createManager(Manager manager);
 }
