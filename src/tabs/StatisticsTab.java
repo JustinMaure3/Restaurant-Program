4353a5b7da8e4473a0226f9b73db51215bc35022
Tomas Markos <tomas.markos01@stclairconnect.ca>
2018-01-04 14:28:07 -0500

    #103 Created the pie chart for October

--- src/tabs/StatisticsTab.java
+++ src/tabs/StatisticsTab.java
@@ -289,11 +289,23 @@ public class StatisticsTab extends Tab{
 	
 	//Pie chart for the month of November
 	public static PieChart novemberPieChart() {
-		int november;
+		ArrayList<Double> itemAmt = new ArrayList();
+		
+		int i = 0;
+		while (i <foodDrinkItems.size()) {
+			itemAmt.add(foodDrinkItems.get(i).getAmountSold());
+		}
 				
 		PieChart pieChart = new PieChart();
 		pieChart.setTitle("November");
 		pieChart.setLabelsVisible(true);
+		
+		ObservableList<PieChart.Data> data = FXCollections.observableArrayList();
+		while (i < foodDrinkItems.size()) {
+			data = FXCollections.observableArrayList(
+					new PieChart.Data((foodDrinkItems.get(i).getName() + ": "), itemAmt.get(i)));
+			i++;
+		}
 		return pieChart;
 		
 	}
