4d51281637b6e1e7e74d13f8d3ada3146683ba0a
Justin Maure <justin.maure01@stclairconnect.ca>
2018-01-06 16:12:35 -0500

    Updated the UpdateTab to functionally work with the foodTable.

--- src/tabs/UpdateItemTab.java
+++ src/tabs/UpdateItemTab.java
@@ -1,7 +1,11 @@
 package tabs;
 
+import PlaceHolder.FoodDrink;
+import Tables.FoodTable;
+import javafx.collections.FXCollections;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
@@ -9,10 +13,13 @@ import javafx.scene.text.Text;
 
 public class UpdateItemTab extends Tab {
 	
+	//Create two booleans to determine if the page displays the update options
+	private Boolean isFoodHidden = true;
+	private Boolean isEmployeeHidden = true;
 	private static UpdateItemTab tab;
 	
 	private UpdateItemTab() {
-		this.setText("Update Item");
+		this.setText("Update");
 		//Create basic layout
 		GridPane pane = new GridPane();
 		
@@ -26,10 +33,87 @@ public class UpdateItemTab extends Tab {
 		pane.add(foodNameText, 0, 1);
 		pane.add(foodName, 1, 1);
 		
+//		__________HIDDEN CATEGORIES________________
+		//Create insert rating row
+		Text ratingText = new Text("Rating:");
+		ComboBox<ENUMS.Ratings> rating = new ComboBox<>();
+		rating.setItems(FXCollections.observableArrayList(ENUMS.Ratings.values()));
+		
+		//Create insert description row
+		Text descText = new Text("Description:");
+		TextField desc = new TextField();
+		
+		//Create insert picture row
+		Text picText = new Text("Picture:");
+		//Add enums for all pictures to choose from?
+		TextField picture = new TextField();
+		
+		//Create insert price row
+		Text priceText = new Text("Price:");
+		TextField price = new TextField();
+		
+		//Create insert amountSold row
+		Text amountSoldText = new Text("Amount Sold:");
+		TextField amountSold = new TextField();
+		
+		//Create insert month row
+		Text monthText = new Text("Month");
+		ComboBox<ENUMS.MonthlyStats> month = new ComboBox<>();
+		month.setItems(FXCollections.observableArrayList(ENUMS.MonthlyStats.values()));
+		
+		
+//		____________HIDDEN CATEGORIES END_____________
+		
+		//if food categories are not hidden show them
+		if(!isFoodHidden) {
+			//Show the rating row
+			pane.add(ratingText, 0, 2);
+			pane.add(rating, 1, 2);
+			
+			//Show the description row
+			pane.add(descText, 0, 3);
+			pane.add(desc, 1, 3);
+			
+			//Show the picture row
+			pane.add(picText, 0, 4);
+			pane.add(picture, 1, 4);
+			
+			//Show the price row
+			pane.add(priceText, 0, 5);
+			pane.add(price, 1, 5);
+			
+			//Show the amountSold row
+			pane.add(amountSoldText, 0, 6);
+			pane.add(amountSold, 1, 6);
+			
+			//Show the month row
+			pane.add(monthText, 0, 7);
+			pane.add(month, 1, 7);
+		}
+		
 		//Create Button for food update
 		Button fUpdate = new Button("Update Food/Drink");
 		fUpdate.setOnAction(e->{
-			
+			if(!foodName.getText().isEmpty()) {
+				if(isFoodHidden) {
+					isFoodHidden = false;
+					pane.add(fUpdate, 1, 8);
+				} else {
+					//Create an instance of the food table
+					FoodTable fTable = new FoodTable();
+					
+					FoodDrink food = new FoodDrink(
+							foodName.getText(),
+							rating.getSelectionModel().getSelectedItem().ordinal(),
+							desc.getText(),
+							picture.getText(),
+							Integer.parseInt(price.getText()) + 0.0,
+							Integer.parseInt(amountSold.getText()) + 0.0,
+							month.getSelectionModel().getSelectedItem().name()
+							);
+					fTable.updateFoodDrink(food);
+				}
+			}
 		});
 		pane.add(fUpdate, 1, 2);
 		
